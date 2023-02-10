7a9a3e65f8a29ba91376efb9be38799a69988c33
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-02-10 19:23:30 +0530

    Day 1/100

---
+++ Arrays/Easy/MissingNumber.cpp
@@ -0,0 +1,27 @@
+// Vaibhav likes to play with numbers and he has N numbers. One day he was placing the numbers on
+// the playing board just to count that how many numbers he has. He was placing the numbers in 
+// increasing order i.e. from 1 to N. But when he was putting the numbers back into his bag, 
+// some numbers fell down onto the floor. He picked up all the numbers but one number, 
+// he couldn't find. Now he has to go somewhere urgently, so he asks you to find the 
+// missing number.
+// NOTE: Don't use Sorting
+
+// Input:                                                       
+// N = 4                                        
+// A[] = {1, 4, 3}
+// Output:
+// 2     
+// Explanation:
+// Vaibhav placed 4 integers but he picked
+// up only 3 numbers. So missing number
+// will be 2 as it will become 1,2,3,4.
+
+int missingNumber(int A[], int N)
+{
+    // Your code goes here
+    int s=0;
+    for(int i=0;i<N-1;i++)
+        s = s + A[i];
+    int sum = (N*(N + 1))/2;
+    return (sum - s);
+}
