4e2221f494a14d9c9eee5aa125eb32bd1ee72f32
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-02-10 19:28:30 +0530

    Day 1/100

---
+++ Arrays/Easy/SingleNumber.cpp
@@ -0,0 +1,22 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 10^4
+// -3 * 104 <= nums[i] <= 3 * 10^4
+// Each element in the array appears twice except for one element which appears only once.
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = nums[0];
+        for(int  i=1;i<nums.size();i++)
+            ans = ans ^ nums[i];
+        return ans;
+    }
+};
